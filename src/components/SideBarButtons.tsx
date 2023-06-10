63f547f48523334dddcc3578bfa1c5ef0805cb4d
Jordan Farelli <jordanfarelli@gmail.com>
2023-06-10 16:00:18 -0600

    Added color-change on active button click

--- src/components/SideBarButtons.tsx
+++ src/components/SideBarButtons.tsx
@@ -1,17 +1,53 @@
-import '../styles/SideBarButtons.css'
+import '../styles/SideBarButtons.css';
 import * as React from 'react';
 import { ButtonGroup, Button } from 'react-bootstrap';
 
 interface SideBarButtonsProps {}
 
 const SideBarButtons: React.FC<SideBarButtonsProps> = (props) => {
+	const [isActive, setIsActive] = React.useState<string | null>(null);
+
+	const handleClick = (buttonId: string) => {
+		setIsActive(buttonId === isActive ? null : buttonId);
+	};
+
 	return (
-		<ButtonGroup vertical className="nav-button-container" size='sm'>
-			<Button className='side-button'>Player Profile</Button>
-			<Button className='side-button'>Tasks</Button>
-			<Button className='side-button'>Events</Button>
-			<Button className='side-button'>Players</Button>
-			<Button className='side-button'>Teams</Button>
+		<ButtonGroup vertical className="nav-button-container" size="sm">
+			<Button
+				id="playerProfileButton"
+				className={isActive === 'playerProfileButton' ? 'active' : ''}
+				onClick={() => handleClick('playerProfileButton')}
+			>
+				Player Profile
+			</Button>
+			<Button
+				id="tasksButton"
+				className={isActive === 'tasksButton' ? 'active' : ''}
+				onClick={() => handleClick('tasksButton')}
+			>
+				Tasks
+			</Button>
+			<Button
+				id="eventsButton"
+				className={isActive === 'eventsButton' ? 'active' : ''}
+				onClick={() => handleClick('eventsButton')}
+			>
+				Events
+			</Button>
+			<Button
+				id="playersButton"
+				className={isActive === 'playersButton' ? 'active' : ''}
+				onClick={() => handleClick('playersButton')}
+			>
+				Players
+			</Button>
+			<Button
+				id="teamsButton"
+				className={isActive === 'teamsButton' ? 'active' : ''}
+				onClick={() => handleClick('teamsButton')}
+			>
+				Teams
+			</Button>
 		</ButtonGroup>
 	);
 };
