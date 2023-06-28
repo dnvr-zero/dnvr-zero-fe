348e7185b47abf5efa77c0e15e988e7513b537c9
Jordan Farelli <jordanfarelli@gmail.com>
2023-06-28 10:16:03 -0600

    Made DNVR-zero logo linked to the LoginPage on mobile view

--- src/components/MobileDropDownMenu/MobileDropDownMenu.tsx
+++ src/components/MobileDropDownMenu/MobileDropDownMenu.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Container, Offcanvas } from 'react-bootstrap';
 import { FaBars } from 'react-icons/fa';
 import SideBarButtons from '../SideBarButtons/SideBarButtons';
+import { Link } from 'react-router-dom';
 
 interface MobileDropDownMenuProps {}
 
@@ -11,20 +12,30 @@ const MobileDropDownMenu: React.FC<MobileDropDownMenuProps> = () => {
 
 	return (
 		<>
-			<Container className="d-flex justify-content-center my-3">
+			<Container className="d-flex align-items-center justify-content-center my-3">
 				<FaBars
 					size={30}
 					className="hamburger-menu d-lg-none"
 					onClick={() => setShow(true)}
 				/>
-				<h2 className="h2-text-on-main-page">
+				<Link
+					to="/"
+					style={{ textDecoration: 'none' }}
+					className="h2-text-on-main-page"
+				>
 					DNVR-<i>zero</i>
-				</h2>
+				</Link>
 			</Container>
 			<Offcanvas show={show} onHide={() => setShow(false)} responsive="lg">
 				<Offcanvas.Header closeButton className="offcanvas-header-container">
 					<Offcanvas.Title className="offcanvas-title-text">
-						DNVR-<i>zero</i>
+						<Link
+							to="/"
+							style={{ textDecoration: 'none', color: 'inherit' }}
+							// className="h2-text-on-main-page"
+						>
+							DNVR-<i>zero</i>
+						</Link>
 					</Offcanvas.Title>
 				</Offcanvas.Header>
 				<Offcanvas.Body className="d-flex flex-column align-items-center">
